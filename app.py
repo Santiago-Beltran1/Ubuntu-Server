8c13e6526a2a9b9c1e8c09d916427e2b40ff4bb7
Santiago-Beltran1 <pedrazasantiago837@gmail.com>
2026-05-26 13:51:08 +0000

    Estrucutura GitHub enlazado con Ubuntu

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, jsonify
+import datetime
+
+app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+placas = ["AAA-000", "BBB-000","CCC-000","DDD-000","EEE-000"]
+
+motos = ["Ducati Panigale","Honda CB500","KTM DUKE 390","NKD 125 :V","Yamaha R15"]
+
+inventario = dict(zip(motos,placas))
+
+@app.route('/api/registros', methods=['GET'])
+def get_registros():
+	return jsonify({
+		"status":"online",
+		"servidor": "Ubuntu de Beltran Pedraza",	
+		"hora_servidor": str(datetime.datetime.now()),
+		"inventario" : inventario
+	})
+
+@app.route('/api/peritajes', methods=['POST'])
+def crear_peritaje():
+	nuevaPlaca = "DSB-212"
+	placas.append(nuevaPlaca)
+	return jsonify({
+		"estado":"Nueva placa registrada de forma correcta",
+		"la placa registrada fue: ": nuevaPlaca
+	})
+
+@app.route('/api/peritajes', methods=['GET'])
+def traer_peritajes():
+	return jsonify({
+		"peritajes": placas
+	})
+
+
+
+if __name__ == "__main__":
+	app.run(host="0.0.0.0", port=5000)
